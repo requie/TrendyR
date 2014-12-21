4a6c96f73ffa5f431afdbce0df68c59e3efc1ef0
Michael Radionov <micrddrgn@gmail.com>
2014-12-21 20:52:13 +0300

    Post-rebase fixes

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20141219085726) do
-=======
-ActiveRecord::Schema.define(version: 20141218130254) do
->>>>>>> added admin page. added ability to select content of homepage
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
