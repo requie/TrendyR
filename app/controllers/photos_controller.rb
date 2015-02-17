56f5554209cbb77b9c110df8888ad30fcebf81c7
Homper <homperator@gmail.com>
2015-02-17 12:46:41 +0300

    Image upload, crop and processing

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,25 @@
+class PhotosController < ApplicationController
+  def create
+    @photo = Photo.new(photo_params)
+    @photo.save
+  end
+
+  def crop
+    @photo = Photo.find(params[:id])
+    @photo.update(crop_photo_params)
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:attachment)
+  end
+
+  def crop_photo_params
+    params.require(:photo).permit(:crop_x, :crop_y, :crop_w, :crop_h)
+  end
+
+  def verify_authorized
+    authorize :base, :access?
+  end
+end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,10 @@ class Photo < ActiveRecord::Base
   delegate :thumb, to: :attachment
   dragonfly_accessor :attachment
 
+  def cropped_photo
+    attachment.thumb("#{crop_w}x#{crop_h}+#{crop_x}+#{crop_y}")
+  end
+
   # redifine the method in child classes to contain presets
   # {
   #   tiny: '100x100#',
