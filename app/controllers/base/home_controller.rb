34ae3ac019b950c8c9ca3e9624dc89e4bdbfe73d
Homper <homperator@gmail.com>
2015-03-06 11:26:21 +0300

    Redirect to home page on log in

--- app/controllers/base/home_controller.rb
+++ app/controllers/base/home_controller.rb
@@ -1,7 +1,38 @@
 module Base
   class HomeController < Base::BaseController
-    def index
-      # renders a template
+    def show
+      authorize @profile
+      @gigs = @profile.owned_gigs.ordered
+      @events = @profile.owned_events.ordered
+      @awards = @profile.owned_awards.ordered
+    end
+
+    def edit
+      authorize @profile
+    end
+
+    def update
+      authorize @profile
+      set_location
+      @profile.update(profile_params)
+      respond_with(@profile, location: base_profile_path)
+    end
+
+    private
+
+    def profile_params
+      params.require(:profile).permit(*policy(@profile).permitted_attributes)
+    end
+
+    def set_location
+      location = Location.find_or_create_by(location_params) do |l|
+        l.creator = @profile.user
+      end
+      @profile.location = location
+    end
+
+    def location_params
+      params.require(:location).permit(*policy(:location).permitted_attributes)
     end
   end
 end
