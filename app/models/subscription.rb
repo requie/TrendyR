0e4a58b65ff3b125c1a9353fabf4b6219549a419
Dima Yatsenka <reiro123@gmail.com>
2015-06-12 11:36:03 +0300

    save emails subscription

---
+++ app/models/subscription.rb
@@ -0,0 +1,4 @@
+class Subscription < ActiveRecord::Base
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :email, presence: true, uniqueness: true
+end
